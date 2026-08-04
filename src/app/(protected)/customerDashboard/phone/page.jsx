8ab5a2027a2a954e523631941a46a6451b484bee
Aleem Ahmad <aleemahmadghias@gmail.com>
2026-08-04 11:39:20 +0500

    aa

---
+++ src/app/(protected)/customerDashboard/phone/page.jsx
@@ -0,0 +1,10 @@
+"use client"
+
+export default function PhonePage() {
+  return (
+    <div className="phone-page">
+      <h1>Phone Page</h1>
+      <p>This is the phone page content.</p>
+    </div>
+  );
+}
